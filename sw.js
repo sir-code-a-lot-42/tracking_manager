4db4ccc0817e67e847e2a4bca668e5134b81d380
sir-code-a-lot-42 <florian.p.lackner@gmail.com>
2026-06-14 19:23:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='gewicht-v7';
+const CACHE='gewicht-v9';
 const SHELL=['index.html','analysis.html','calories.html','calories-analysis.html',
-  'training.html','config.js','foods.js','manifest.json','icon-180.png','icon-192.png','icon-512.png'];
+  'training.html','strava.html','config.js','foods.js','manifest.json','icon-180.png','icon-192.png','icon-512.png'];
 
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
